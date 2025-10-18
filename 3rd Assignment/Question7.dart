c2991165f8199a85a983a50aa26ddcf968fc810c
istiaqrahmanmahi <cse_0182320012101062@lus.ac.bd>
2025-10-18 22:28:44 +0600

    ID-0182320012101062. Name-Istiaq Rahman Mahi

---
+++ 3rd Assignment/Question7.dart
@@ -0,0 +1,21 @@
+
+
+//Create a map with name, phone keys and store some values to it.
+// Use where to find all keys that have length 4
+
+
+void main() {
+  Map<String, String> contacts = {
+    'Mahi': '12345',
+    'Mahdi': '98765',
+    'Annesha': '54321',
+    'Juii': '67890'
+  };
+
+  var keysWithLength4 = contacts.keys.where((key) => key.length == 4);
+
+  print("Keys with length 4:");
+  for (var key in keysWithLength4) {
+    print(key);
+  }
+}
---
+++ 3rd Assignment/Question_1.dart
@@ -0,0 +1,12 @@
+
+
+//Create a list of names and print all names using list.
+
+void main() {
+  List<String> names = ['mahi', 'rahi', 'shahi', 'nahi'];
+  
+  print("All names:");
+  for (var name in names) {
+    print(name);
+  }
+}
